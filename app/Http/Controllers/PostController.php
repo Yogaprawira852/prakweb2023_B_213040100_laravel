35e71d6f0e0f9b4fc0e3b5275f66f2d942711293
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-12 15:35:37 +0700

    menambahkan catatan laravel di youtube #13

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Models\Post;
-
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('posts', [
-            "title" => "Posts",
-            "posts" => Post::All()
-        ]);
+            "title"=> "Posts",
+            "posts"=> Post::all()
+
+    ]);
     }
 
-    public function show(Post $post) {
+    public function show($slug)
+    {
+        
         return view('post', [
-            "title" => "Single Post",
-            "post" => $post
+            "title"=> "Single Post",
+            "post" => Post::find($slug)
         ]);
+
     }
+
+    
 }
