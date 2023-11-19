d5cf744868c954dbb2f7407af405c8a64e9024ad
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-19 17:40:53 +0700

    video 17

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,3 +45,17 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest'); // yang bisa mengakses register adalah guest
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+}) ->middleware('auth'); // yg bisa akses yang telah terautentikasi
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->
+middleware('auth');
