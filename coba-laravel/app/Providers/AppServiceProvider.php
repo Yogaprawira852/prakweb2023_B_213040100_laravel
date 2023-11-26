58214fe287d4368d04d842e25187dbcec0d812dc
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-26 15:25:42 +0700

    video 23

--- coba-laravel/app/Providers/AppServiceProvider.php
+++ coba-laravel/app/Providers/AppServiceProvider.php
@@ -4,26 +4,28 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
-    {
+    public function boot(): void {
         Paginator::useBootstrap();
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
     }
 }
