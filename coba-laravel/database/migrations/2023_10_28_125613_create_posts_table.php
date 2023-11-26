318fd11f63515011da8a0705a03271c1b924f171
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-26 15:29:00 +0700

    Video 21

--- coba-laravel/database/migrations/2023_10_28_125613_create_posts_table.php
+++ coba-laravel/database/migrations/2023_10_28_125613_create_posts_table.php
@@ -9,12 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+			$table->foreignId('category_id');
+            $table->foreignId('user_id');
             $table->string('title');
             $table->string('slug')->unique();
+            $table->string('image')->nullable();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamp("published_at")->nullable();
