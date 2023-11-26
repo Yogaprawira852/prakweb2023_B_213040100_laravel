fb594aa0cc28285fdbaf326cd89e9ba03341a547
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-26 15:35:43 +0700

    Video 23

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Models\Category;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardPostController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,49 +11,47 @@ use App\Http\Controllers\DashboardPostController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home",
-        "active" => "home"
+    return view ('home', [
+        "title"=> "Home"
+        
     ]);
 });
+
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
-        "active" => "about",
+        "title"=> "About",
         "name" => "Yoga Prawira Kusuma",
-        "email" => "Yoga@unpas.ac.id",
-        "image" => "image.jpg"
+        "email" => "yprawira852@gmail.com",
+        "img" => "image.jpg"
     ]);
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function(){
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/categories', function() {
     return view('categories', [
-        'title' => 'Post Categories',
+        'title' => "Post by Categories",
         'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
-
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
-
-Route::get('/register', [RegisterController::class, 'index'])->middleware('guest'); // yang bisa mengakses register adalah guest
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
-
-Route::get('/dashboard', function() {
+Route::get('/dashboard', function () {
     return view('dashboard.index');
-}) ->middleware('auth'); // yg bisa akses yang telah terautentikasi
-
-Route::resource('/dashboard/posts', DashboardPostController::class)->
-middleware('auth');
+})->middleware('auth');
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
