6b7807ed348af7310ec185c51ed4050216449f4e
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-26 15:27:32 +0700

    Video 21

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,28 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory(5)->create();
+        User::create([
+            'name' => 'Yoga Prawira Kussuma',
+            'username' => 'Yoga Prawira Kusuma',
+            'email' => 'yoga@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-Design'
+        ]);
+
+        Post::factory(20)->create();
     }
 }
