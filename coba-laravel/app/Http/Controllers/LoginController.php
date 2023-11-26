f69cda575384c289ebe2bd402c8ea950ed30f020
Yoga Prawira Kusuma <96771743+Yogaprawira852@users.noreply.github.com>
2023-11-26 15:22:29 +0700

    video 21

--- coba-laravel/app/Http/Controllers/LoginController.php
+++ coba-laravel/app/Http/Controllers/LoginController.php
@@ -1,34 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function index() {
         return view('login.index', [
             'title' => 'Login',
-            'active'=>'login'
+            'active' => 'login'
         ]);
     }
-
     public function authenticate(Request $request) {
         $credentials = $request->validate([
-            'email' => 'required|email:dns',
+            'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)) {
-            $request -> session()->regenerate();
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
             return redirect()->intended('/dashboard');
         }
 
         return back()->with('loginError', 'Login failed!');
     }
 
-    public function logout() {
+    public function logout(Request $request) {
         Auth::logout();
 
         request()->session()->invalidate();
